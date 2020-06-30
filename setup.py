33c8d2ec623f8b0a9e1a7041047d1a1619e03d67
wbeasley <wbeasley@cylance.com>
2020-06-29 20:57:47 -0500

    Added requirements and setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name='nab3',
+    version='0.1.0',
+    description='Easily inspect AWS resources',
+    long_description='Easily inspect AWS resources',
+    url='https://github.com/WillNye/nab3',
+    python_requires='>=3.7',
+    install_requires=[
+        'boto3<2.0.0',
+    ],
+    packages=find_namespace_packages(include=['nab3', 'nab3.*']),
+    package_data={'': ['*.md']},
+    include_package_data=True,
+    author='Will Beasley',
+    author_email='willbeas88@gmail.com',
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
