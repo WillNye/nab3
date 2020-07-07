79e00f8df81b19378c9e7866205dfbe00a3131f0
wbeasley <wbeasley@cylance.com>
2020-07-07 11:04:25 -0500

    Cleanup helper functions. Moved alerts into dedicated alerts module

--- nab3/helpers/ec2.py
+++ nab3/helpers/ec2.py
@@ -29,20 +29,6 @@ def display_asg_sgs(asg_object):
     echo(f"# {asg_object.name} Security Groups\n{generate_md_table_str(row_list=rows, headers=headers)}")
 
 
-def display_asg_alerts(asg_object, start_date=dt.now()-timedelta(days=30), end_date=dt.now()):
-    md_output = f'# {asg_object.name}\n## Policies:\n'
-    policy_summary = []
-    headers = ['Policy', 'Alarm', 'Time']
-    rows = []
-    for asp in tqdm(asg_object.scaling_policies):
-        alerts = asp.get_alerts(start_date=start_date, end_date=end_date, item_type='Action')
-        policy_summary.append(f'{asp.name} - {len(alerts)}')
-        rows += [[asp.name, alert.name, alert.timestamp] for alert in alerts]
-
-    md_output += f"{generate_md_bullet_str(policy_summary)}{generate_md_table_str(row_list=rows, headers=headers)}"
-    echo(md_output)
-
-
 def display_asg_ips(asg_object):
     headers = ['Instance ID', 'IP', 'State']
     rows = []
--- nab3/helpers/ecs.py
+++ nab3/helpers/ecs.py
@@ -1,11 +1,14 @@
 from double_click import echo
 from double_click.markdown import generate_md_bullet_str, generate_md_table_str
 
+from nab3.helpers.alerts import generated_md_alerts
 
-def display_ecs_service_summary(ecs_service):
+
+def md_ecs_service_summary(ecs_service, display_alerts=True, display_events=True):
     """
 
     :param ecs_service: ECSService object
+    :param display_events: bool Default(True) - Display the 50 most recent events of the service
     :return:
     """
     ecs_service.load()
@@ -18,7 +21,34 @@ def display_ecs_service_summary(ecs_service):
         f"Running Count: {ecs_service.running_count}",
         f"Pending Count: {ecs_service.pending_count}"
     ]
-    md_output = f"# {ecs_service.name}\n{generate_md_bullet_str(bullets)}\n## Events"
-    headers = ["Event", "Occurred At"]
-    rows = [[event.get('message'), event.get('created_at')] for event in ecs_service.events]
-    echo(f"{md_output}{generate_md_table_str(rows, headers)}")
+    md_output = f"# {ecs_service.name}\n{generate_md_bullet_str(bullets)}"
+    if display_events:
+        headers = ["Event", "Occurred At"]
+        rows = [[event.get('message'), event.get('created_at')] for event in ecs_service.events]
+        md_output += f"\n## Events{generate_md_table_str(rows, headers)}"
+
+    if display_alerts:
+        md_output += generated_md_alerts(ecs_service, include_name=False)
+
+    return md_output
+
+
+def display_ecs_service_summary(ecs_service, display_alerts=True, display_events=True):
+    """
+
+    :param ecs_service: ECSService object
+    :param display_events: bool Default(True) - Display the 50 most recent events of the service
+    :return:
+    """
+    echo(md_ecs_service_summary(ecs_service, display_alerts, display_events))
+
+
+def display_ecs_cluster_summary(ecs_cluster, display_cloudwatch_alarms=False, display_service_events=False):
+    """
+
+    :param ecs_cluster: ECSCluster object
+    :param display_cloudwatch_alarms: bool Default(False) - Display cluster cloudwatch alarms for the last 30 days
+    :param display_service_events: bool Default(False) - Display the 50 most recent events of each service
+    :return:
+    """
+    ecs_cluster.load()
