0fca05ef9a7347307d8f61c0bd6af249f664c284
wbeasley <wbeasley@cylance.com>
2020-07-22 08:38:04 -0500

    alert -> alarm to align with AWS naming. Moved mixins to a dedicated module.

--- nab3/service.py
+++ nab3/service.py
@@ -1,125 +1,13 @@
 import base64
 import logging
 from itertools import chain
-from datetime import datetime as dt, timedelta
 
+from nab3.mixin import AppAutoScaleMixin, AutoScaleMixin, MetricMixin, SecurityGroupMixin
 from nab3.base import BaseService, PaginatedBaseService
 from nab3.utils import paginated_search, snake_to_camelcap
 
 LOGGER = logging.getLogger('nab3')
 LOGGER.setLevel(logging.WARNING)
-
-
-class AppAutoScaleMixin:
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.create_service_field('scaling_policies', 'app_scaling_policy')
-
-    @property
-    def resource_id(self):
-        raise NotImplementedError
-
-    async def load_scaling_policies(self):
-        if self.scaling_policies.loaded:
-            return self.scaling_policies
-
-        asp_list = await self.scaling_policies.list(resource_id=self.resource_id)
-        self.scaling_policies = asp_list
-        return self.scaling_policies
-
-
-class AutoScaleMixin:
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.create_service_field('scaling_policies', 'scaling_policy')
-
-    async def load_scaling_policies(self):
-        if not self.scaling_policies.loaded:
-            self.scaling_policies = await self.scaling_policies.list(asg_name=self.name)
-
-
-class SecurityGroupMixin:
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.create_service_field('accessible_resources', 'security_group')
-        self.create_service_field('security_groups', 'security_group')
-
-    async def load_accessible_resources(self):
-        if self.accessible_resources.loaded:
-            return self.accessible_resources
-
-        if not self.security_groups.loaded:
-            await self.fetch('security_groups')
-
-        filter_list = [sg.id for sg in self.security_groups]
-        if not filter_list:
-            return self.accessible_resources
-
-        self.accessible_resources = await self.accessible_resources.service_class.list(Filters=[dict(
-            Name='ip-permission.group-id',
-            Values=filter_list
-        )])
-
-        return self.accessible_resources
-
-
-class MetricMixin:
-    _available_metrics = False
-
-    def get_statistics(self,
-                       metric_name: str,
-                       start_time: dt = dt.utcnow()-timedelta(hours=3),
-                       end_time: dt = dt.utcnow(),
-                       interval_as_seconds: int = 300, **kwargs) -> list:
-        """
-        :param metric_name:
-        :param start_time:
-        :param end_time:
-        :param interval_as_seconds:
-        :param kwargs:
-        :return:
-        """
-        kwargs = {snake_to_camelcap(k): v for k, v in kwargs.items()}
-        dimensions = self._stat_dimensions + kwargs.get('Dimensions', [])
-        kwargs['Dimensions'] = dimensions
-
-        if kwargs.get('ExtendedStatistics') is False and kwargs.get('Statistics') is False:
-            LOGGER.warning('Neither ExtendedStatistics or Statistics was set. Defaulting to Statistics=[Average]')
-            kwargs['Statistics'] = ['Average']
-
-        metric_cls = self._get_service_class('metric')
-        metrics = metric_cls.get_statistics(
-            self._stat_name, metric_name, start_time, end_time, interval_as_seconds, **kwargs
-        )
-        return metrics
-
-    async def get_available_metrics(self):
-        if self._available_metrics is False:
-            metrics = self._get_service_class('metric')
-            metrics = await metrics.list(Namespace=self._stat_name, Dimensions=self._stat_dimensions)
-            self._available_metrics = metrics
-        return self._available_metrics
-
-    async def get_metric_options(self):
-        if self._available_metrics is False:
-            metrics = self._get_service_class('metric')
-            metrics = await metrics.list(Namespace=self._stat_name, Dimensions=self._stat_dimensions)
-            self._available_metrics = metrics
-        return set(metric.name for metric in self._available_metrics)
-
-    @property
-    def _stat_dimensions(self) -> list:
-        raise NotImplementedError
-
-    @property
-    def _stat_name(self) -> str:
-        """
-        docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/aws-services-cloudwatch-metrics.html
-        :return:
-        """
-        raise NotImplementedError
 
 
 class Alarm(PaginatedBaseService):
@@ -232,7 +120,7 @@ class AutoScalePolicy(PaginatedBaseService):
         response_key='ScalingPolicies'
     )
 
-    def get_alerts(self, start_date, end_date, item_type=None, alarm_types=[], sort_desc=True):
+    def get_alarms(self, start_date, end_date, item_type=None, alarm_types=[], sort_desc=True):
         """
         :param start_date: StartDate=datetime(2015, 1, 1)
         :param end_date: EndDate=datetime(2015, 1, 1)
@@ -241,14 +129,14 @@ class AutoScalePolicy(PaginatedBaseService):
         :param sort_desc: bool -> ScanBy='TimestampDescending TimestampAscending'
         :return:
         """
-        alerts = []
-        alert_obj = self._get_service_class('alarm')
+        alarms = []
+        alarm_obj = self._get_service_class('alarm')
 
-        for alert in self.alarms:
-            alerts += alert_obj.get_history(name=alert.name, start_date=start_date, end_date=end_date,
+        for alarm in self.alarms:
+            alarms += alarm_obj.get_history(name=alarm.name, start_date=start_date, end_date=end_date,
                                             item_type=item_type, alarm_types=alarm_types, sort_descending=sort_desc)
 
-        return alerts
+        return alarms
 
 
 class AppAutoScalePolicy(PaginatedBaseService):
@@ -278,13 +166,14 @@ class AppAutoScalePolicy(PaginatedBaseService):
         client_call='describe_scaling_policies',
         call_params=dict(
             scalable_dimension=dict(name='ScalableDimension', type=str),
+            service_namespace=dict(name='ServiceNamespace', type=str),
             name=dict(name='PolicyNames', type=list),
             resource_id=dict(name='ResourceId', type=str)
         ),
         response_key='ScalingPolicies'
     )
 
-    def get_alerts(self, start_date, end_date, item_type=None, alarm_types=[], sort_desc=True):
+    def get_alarms(self, start_date, end_date, item_type=None, alarm_types=[], sort_desc=True):
         """
         :param start_date: StartDate=datetime(2015, 1, 1)
         :param end_date: EndDate=datetime(2015, 1, 1)
@@ -293,13 +182,13 @@ class AppAutoScalePolicy(PaginatedBaseService):
         :param sort_desc: bool -> ScanBy='TimestampDescending TimestampAscending'
         :return:
         """
-        alerts = []
-        alert_obj = self._get_service_class('alarm')
-        for alert in self.alarms:
-            alerts += alert_obj.get_history(name=alert.name, start_date=start_date, end_date=end_date,
+        alarms = []
+        alarm_obj = self._get_service_class('alarm')
+        for alarm in self.alarms:
+            alarms += alarm_obj.get_history(name=alarm.name, start_date=start_date, end_date=end_date,
                                             item_type=item_type, alarm_types=alarm_types, sort_descending=sort_desc)
 
-        return alerts
+        return alarms
 
 
 class SecurityGroup(PaginatedBaseService):
