d7bbc3f0ccf3e4e7a8a762028ef53211524253c8
wbeasley <wbeasley@cylance.com>
2020-09-28 09:06:21 -0500

    Improved normalization to the pricing service response

--- nab3/service/pricing.py
+++ nab3/service/pricing.py
@@ -1,6 +1,11 @@
+import json
 import logging
 
 from nab3.base import PaginatedBaseService
+
+from nab3.utils import (
+    camel_to_snake, paginated_search, snake_to_camelcap
+)
 
 LOGGER = logging.getLogger('nab3')
 LOGGER.setLevel(logging.WARNING)
@@ -12,6 +17,7 @@ class Pricing(PaginatedBaseService):
     """
     boto3_client_name = 'pricing'
     key_prefix = 'product'
+    _to_boto3_case = snake_to_camelcap
     _boto3_describe_def = dict(
         client_call='get_products',
         call_params=dict(
@@ -21,3 +27,38 @@ class Pricing(PaginatedBaseService):
         response_key='PriceList'
     )
 
+    def get_on_demand_hourly(self, currency='usd'):
+        pricing = list(list(self.on_demand.values())[0]["price_dimensions"].values())[0]
+        return float(pricing['price_per_unit'][currency])
+
+    def get_on_demand_monthly(self, currency='usd'):
+        return self.get_on_demand_hourly(currency) * 24 * 30
+
+    @classmethod
+    async def _list(cls, **kwargs) -> list:
+        """Returns an instance for each object
+        JMESPath for filtering: https://jmespath.org
+        :param kwargs:
+        :return: list<cls()>
+        """
+        kwargs = {cls._to_boto3_case(k): v for k, v in kwargs.items() if k not in ['list_kwargs', 'describe_kwargs']}
+        response_key = cls._boto3_describe_def.get('response_key', f'{cls.key_prefix}s')
+        fnc_base = camel_to_snake(cls.key_prefix)
+        fnc_name = cls._boto3_describe_def.get('client_call', f'describe_{fnc_base}s')
+        return_val = []
+
+        client = cls._client.get(cls.boto3_client_name)
+        boto3_fnc = getattr(client, fnc_name)
+        response = paginated_search(boto3_fnc, kwargs, response_key)
+        for obj in response:
+            loaded_obj = json.loads(obj)
+            attributes = loaded_obj['product']['attributes']
+            for k, v in attributes.items():
+                if attributes[k] == 'Yes':
+                    attributes[k] = True
+                elif attributes[k] == 'No':
+                    attributes[k] = False
+
+            return_val.append(dict(**loaded_obj['terms'], **attributes))
+
+        return [cls(_loaded=True, **obj) for obj in return_val]
--- nab3/utils.py
+++ nab3/utils.py
@@ -1,6 +1,28 @@
 import asyncio
 import re
 
+PRICING_REGION_MAP = {
+            'us-east-1': 'US East (N. Virginia)',
+            'us-east-2': 'US East (Ohio)',
+            'us-west-1': 'US West (N. California)',
+            'us-west-2': 'US West (Oregon)',
+            'ca-central-1': 'Canada (Central)',
+            'eu-north-1': 'EU (Stockholm)',
+            'eu-west-1': 'EU (Ireland)',
+            'eu-central-1': 'EU (Frankfurt)',
+            'eu-west-2': 'EU (London)',
+            'eu-west-3': 'EU (Paris)',
+            'ap-northeast-1': 'Asia Pacific (Tokyo)',
+            'ap-northeast-2': 'Asia Pacific (Seoul)',
+            'ap-northeast-3': 'Asia Pacific (Osaka-Local)',
+            'ap-southeast-1': 'Asia Pacific (Singapore)',
+            'ap-southeast-2': 'Asia Pacific (Sydney)',
+            'ap-south-1': 'Asia Pacific (Mumbai)',
+            'sa-east-1': 'South America (Sao Paulo)',  # intentionally no unicode
+            'us-gov-west-1': 'AWS GovCloud (US)',
+            'us-gov-east-1': 'AWS GovCloud (US-East)'
+        }
+
 
 def camel_to_snake(str_obj: str) -> str:
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', str_obj).lower()
